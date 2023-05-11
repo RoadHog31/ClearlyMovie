82fdaf287a03f957c7175fb740c537d2a72d1812
Stephen Pino~ <you@example.com>
2023-05-11 11:47:47 +0100

    Add rating field.

--- Migrations/20230511104546_Rating.Designer.cs
+++ Migrations/20230511104546_Rating.Designer.cs
@@ -4,6 +4,7 @@ using ClearlyMovie.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClearlyMovie.Migrations
 {
     [DbContext(typeof(ClearlyMovieContext))]
-    partial class ClearlyMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20230511104546_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
